7f4cadd53fd15b2f830f116484a6250a1cf2a809
qbll9 <qmaccba@gmail.com>
2024-05-24 09:09:59 +0200

    refactor(db_tests): Change structure of modules and use statements.

--- database/src/db_tests.rs
+++ database/src/db_tests.rs
@@ -1,108 +1,102 @@
 #[cfg(test)]
-mod tests {
-    // use super::*;
+use crate::{
+    models::{DBState, Epic, Status, Story},
+    Database, JSONFileDatabase,
+};
+use std::collections::HashMap;
+use std::io::Write;
 
-    mod database {
-        use std::collections::HashMap;
-        use std::io::Write;
+#[test]
+fn read_db_should_fail_with_invalid_path() {
+    let db = JSONFileDatabase {
+        file_path: "INVALID_PATH".to_owned(),
+    };
+    assert_eq!(db.read_db().is_err(), true);
+}
 
-        // use super::*;
-        use crate::*;
-        use models::{DBState, Epic, Status, Story};
+#[test]
+fn read_db_should_fail_with_invalid_json() {
+    let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
 
-        #[test]
-        fn read_db_should_fail_with_invalid_path() {
-            let db = JSONFileDatabase {
-                file_path: "INVALID_PATH".to_owned(),
-            };
-            assert_eq!(db.read_db().is_err(), true);
-        }
+    let file_contents = r#"{ "last_item_id": 0 epics: {} stories {} }"#;
+    write!(tmpfile, "{}", file_contents).unwrap();
 
-        #[test]
-        fn read_db_should_fail_with_invalid_json() {
-            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+    let db = JSONFileDatabase {
+        file_path: tmpfile
+            .path()
+            .to_str()
+            .expect("failed to convert tmpfile path to str")
+            .to_string(),
+    };
 
-            let file_contents = r#"{ "last_item_id": 0 epics: {} stories {} }"#;
-            write!(tmpfile, "{}", file_contents).unwrap();
+    let result = db.read_db();
 
-            let db = JSONFileDatabase {
-                file_path: tmpfile
-                    .path()
-                    .to_str()
-                    .expect("failed to convert tmpfile path to str")
-                    .to_string(),
-            };
+    assert_eq!(result.is_err(), true);
+}
 
-            let result = db.read_db();
+#[test]
+fn read_db_should_parse_json_file() {
+    let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
 
-            assert_eq!(result.is_err(), true);
-        }
+    let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+    write!(tmpfile, "{}", file_contents).unwrap();
 
-        #[test]
-        fn read_db_should_parse_json_file() {
-            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+    let db = JSONFileDatabase {
+        file_path: tmpfile
+            .path()
+            .to_str()
+            .expect("failed to convert tmpfile path to str")
+            .to_string(),
+    };
 
-            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
-            write!(tmpfile, "{}", file_contents).unwrap();
+    let result = db.read_db();
 
-            let db = JSONFileDatabase {
-                file_path: tmpfile
-                    .path()
-                    .to_str()
-                    .expect("failed to convert tmpfile path to str")
-                    .to_string(),
-            };
+    assert_eq!(result.is_ok(), true);
+}
 
-            let result = db.read_db();
+#[test]
+fn write_db_should_work() {
+    let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
 
-            assert_eq!(result.is_ok(), true);
-        }
+    let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+    write!(tmpfile, "{}", file_contents).unwrap();
 
-        #[test]
-        fn write_db_should_work() {
-            let mut tmpfile = tempfile::NamedTempFile::new().unwrap();
+    let db = JSONFileDatabase {
+        file_path: tmpfile
+            .path()
+            .to_str()
+            .expect("failed to convert tmpfile path to str")
+            .to_string(),
+    };
 
-            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
-            write!(tmpfile, "{}", file_contents).unwrap();
+    let story = Story {
+        name: "epic 1".to_owned(),
+        description: "epic 1".to_owned(),
+        status: Status::Open,
+    };
+    let epic = Epic {
+        name: "epic 1".to_owned(),
+        description: "epic 1".to_owned(),
+        status: Status::Open,
+        stories: vec![2],
+    };
 
-            let db = JSONFileDatabase {
-                file_path: tmpfile
-                    .path()
-                    .to_str()
-                    .expect("failed to convert tmpfile path to str")
-                    .to_string(),
-            };
+    let mut stories = HashMap::new();
+    stories.insert(2, story);
 
-            let story = Story {
-                name: "epic 1".to_owned(),
-                description: "epic 1".to_owned(),
-                status: Status::Open,
-            };
-            let epic = Epic {
-                name: "epic 1".to_owned(),
-                description: "epic 1".to_owned(),
-                status: Status::Open,
-                stories: vec![2],
-            };
+    let mut epics = HashMap::new();
+    epics.insert(1, epic);
 
-            let mut stories = HashMap::new();
-            stories.insert(2, story);
+    let state = DBState {
+        last_item_id: 2,
+        epics,
+        stories,
+    };
 
-            let mut epics = HashMap::new();
-            epics.insert(1, epic);
+    let write_result = db.write_db(&state);
+    let read_result = db.read_db().unwrap();
 
-            let state = DBState {
-                last_item_id: 2,
-                epics,
-                stories,
-            };
-
-            let write_result = db.write_db(&state);
-            let read_result = db.read_db().unwrap();
-
-            assert_eq!(write_result.is_ok(), true);
-            // TODO: fix this error by deriving the appropriate traits for DBState
-            assert_eq!(read_result, state);
-        }
-    }
+    assert_eq!(write_result.is_ok(), true);
+    // TODO: fix this error by deriving the appropriate traits for DBState
+    assert_eq!(read_result, state);
 }
